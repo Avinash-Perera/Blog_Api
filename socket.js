aaaae39a6c405c984faec144f1d8470e94f1e262
AvinashPerera00 <it20246914@my.sliit.lk>
2023-07-15 11:08:07 +0530

    first commit

---
+++ socket.js
@@ -0,0 +1,34 @@
+// let io;
+
+// module.exports = {
+//   init: httpServer => {
+//     io = require('socket.io')(httpServer);
+//     return io;
+//   },
+//   getIO: () => {
+//     if (!io) {
+//       throw new Error('Socket.io not initialized!');
+//     }
+//     return io;
+//   }
+// };
+let io;
+ 
+module.exports = {
+  init: httpServer => {
+      io = require("socket.io")(httpServer, {
+         cors: {
+             origin: "http://localhost:3000",
+             methods: ["GET", "POST", "PUT", "DELETE"]
+         }
+     });
+      
+    return io;
+  },
+  getIO: () => {
+    if (!io) {
+      throw new Error("Socket.io not initialized!");
+    }
+    return io;
+  },
+};
